6c9fba8229acd06604d5521f5989ddd5bee02176
Raul Antonio Chavez Aguirre <54995852+xXChAvE2Xx@users.noreply.github.com>
2022-06-14 17:18:23 -0500

    Se edito la estuctura del codigo para mostrar las ternerar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,22 +3,25 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-12">
-           <div class="card" style="width: 18rem;">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Example_image.svg/600px-Example_image.svg.png" class="card-img-top" alt="Ternera">
-                <div class="card-body">
-                    <h5 class="card-title">Nombre</h5>
-                    <p class="card-text">Descripcion de la Ternera</p>
+        <div class="col-md-12 d-flex flex-nowrap">
+            @foreach ($breedings as $breeding)
+                <div class="card" style="width: 18rem; margin-right:15px;">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Example_image.svg/600px-Example_image.svg.png" class="card-img-top" alt="Ternera">
+                    <div class="card-body">
+                        <h5 class="card-title">{{$breeding->color_muscle}}</h5>
+                        <p class="card-text">{{$breeding->description}}</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Costo: {{$breeding->cost}} pesos</li>
+                        <li class="list-group-item">Peso: {{$breeding->weight}} kg</li>
+                        <li class="list-group-item">Grasa tipo {{$breeding->color_muscle}}</li>
+                        <li class="list-group-item">Grasa tipo {{$breeding->color_muscle}}</li>
+                    </ul>
+                    <div class="card-body">
+                        <a href="#" class="card-link">Enferma</a>
+                    </div>
                 </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">Costo: $1,589 pesos</li>
-                    <li class="list-group-item">Peso: 185 kg</li>
-                    <li class="list-group-item">Grasa tipo 1</li>
-                </ul>
-                <div class="card-body">
-                    <a href="#" class="card-link">Enferma</a>
-                </div>
-            </div>
+            @endforeach
         </div>
     </div>
 </div>
