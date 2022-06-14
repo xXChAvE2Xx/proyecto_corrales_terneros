991baa5535ece88846137f3e1f2469aab49d4c3e
Raul Antonio Chavez Aguirre <54995852+xXChAvE2Xx@users.noreply.github.com>
2022-06-14 15:35:01 -0500

    Se creo el modelo y migracion para la tabla posision y la llave foranea

---
+++ database/migrations/2022_06_14_194043_add_position_id_to_users.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->bigInteger('id_position')->unsigned();
+            $table->foreign('id_position')
+                  ->references('id')
+                  ->on('positions');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
