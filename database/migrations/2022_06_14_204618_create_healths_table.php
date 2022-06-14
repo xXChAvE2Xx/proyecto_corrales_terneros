dfa607a5975599c16ef74f195d6483fc66a9e3a4
Raul Antonio Chavez Aguirre <54995852+xXChAvE2Xx@users.noreply.github.com>
2022-06-14 15:47:57 -0500

    Se creo el modelo y migracion para la tabla salud

---
+++ database/migrations/2022_06_14_204618_create_healths_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('healths', function (Blueprint $table) {
+            $table->id();
+            $table->float('temperature');
+            $table->integer('heart_frecuency');
+            $table->integer('breathing_rate');
+            $table->integer('blood_pressure');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('healths');
+    }
+};
