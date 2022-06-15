2e9932945e2bd3483cdf74b92779ef06c8a02b3d
Raul Antonio Chavez Aguirre <54995852+xXChAvE2Xx@users.noreply.github.com>
2022-06-14 21:19:42 -0500

    Se agrego la ruta solo para mostrar toda la informacion

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,10 @@ Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('ho
 Route::get('/breeding', [App\Http\Controllers\BreedingController::class, 'index'])->name('breeding.index')->middleware('auth');
 Route::post('/breeding', [App\Http\Controllers\BreedingController::class, 'store'])->name('breeding.store');
 
-Route::get('/edit/{id}', [App\Http\Controllers\BreedingController::class, 'show'])->name('breeding.edit');
+Route::get('/edit/{id}', [App\Http\Controllers\BreedingController::class, 'edit'])->name('breeding.edit');
 Route::patch('/edit/{id}', [App\Http\Controllers\BreedingController::class, 'update'])->name('breeding.update');
 
+Route::get('/show/{id}', [App\Http\Controllers\BreedingController::class, 'show'])->name('breeding.show');
 
 Route::get('/register', [App\Http\Controllers\Auth\RegisterController::class, 'index'])->name('register');
 
