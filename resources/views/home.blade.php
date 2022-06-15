da3bf1342702f2e0316073e75326c68f70773297
Raul Antonio Chavez Aguirre <54995852+xXChAvE2Xx@users.noreply.github.com>
2022-06-15 00:11:26 -0500

    Se agrego ruta para editar y mostrar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,13 +2,15 @@
 
 @section('content')
 <div class="container">
+    @if (session('success'))
+        <h6 class="alert alert-success">{{ session('success') }}</h6> 
+    @endif
     <div class="row justify-content-center">
         <div class="col-md-12 d-flex flex-nowrap">
             @foreach ($breedings as $breeding)
                 <div class="card" style="width: 18rem; margin-right:15px;">
                     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Example_image.svg/600px-Example_image.svg.png" class="card-img-top" alt="Ternera">
                     <div class="card-body">
-                        <h5 class="card-title">{{$breeding->color_muscle}}</h5>
                         <p class="card-text">{{$breeding->description}}</p>
                     </div>
                     <ul class="list-group list-group-flush">
@@ -18,8 +20,25 @@
                         <li class="list-group-item">Grasa tipo {{$breeding->color_muscle}}</li>
                     </ul>
                     <div class="card-body">
-                        <a href="#" class="card-link">Enferma</a>
-                    </div>
+                        <div class="btn-group-vertical justify-content-center" role="group">
+                            <a href="{{ route('breeding.show', ['id' => $breeding->id]) }}" class="btn btn-primary">Ver más información</a>
+
+                            @if (Auth::user()->id_position == 2)
+                                <a href="" class="btn btn-warning">Marcar como enferma</a>
+                            @endif
+
+                            @if (Auth::user()->id_position == 1)
+                                <a href="{{ route('breeding.edit', ['id' => $breeding->id]) }}" class="btn btn-secondary"><i class="fa fa-pencil-square-o"></i> Editar</a>
+                            @endif
+
+                            @if (Auth::user()->id_position == 3)
+                                <a href="#" class="btn btn-info">Registrar datos de sensores</a>
+                            @endif
+                        </div>
+                        
+                    </div>  
+
+                    
                 </div>
             @endforeach
         </div>
