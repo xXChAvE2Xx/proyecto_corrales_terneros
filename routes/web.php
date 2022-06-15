4f982b1e349f88bbc1a59c40aa859b92118bf89d
Raul Antonio Chavez Aguirre <54995852+xXChAvE2Xx@users.noreply.github.com>
2022-06-15 03:02:03 -0500

    Se borro el doble punto y coma

--- routes/web.php
+++ routes/web.php
@@ -20,13 +20,13 @@ Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('ho
 Route::get('/breeding', [App\Http\Controllers\BreedingController::class, 'index'])->name('breeding.index')->middleware('auth');
 Route::post('/breeding', [App\Http\Controllers\BreedingController::class, 'store'])->name('breeding.store');
 
-Route::get('/breeding/{id}', [App\Http\Controllers\QuarantineController::class, 'index'])->name('quarantine.index')->middleware('auth');;
-Route::patch('/breeding/{id}', [App\Http\Controllers\QuarantineController::class, 'update'])->name('quarantine.update')->middleware('auth');;
+Route::get('/breeding/{id}', [App\Http\Controllers\QuarantineController::class, 'index'])->name('quarantine.index')->middleware('auth');
+Route::patch('/breeding/{id}', [App\Http\Controllers\QuarantineController::class, 'update'])->name('quarantine.update')->middleware('auth');
 
-Route::get('/edit/{id}', [App\Http\Controllers\BreedingController::class, 'edit'])->name('breeding.edit')->middleware('auth');;
+Route::get('/edit/{id}', [App\Http\Controllers\BreedingController::class, 'edit'])->name('breeding.edit')->middleware('auth');
 Route::patch('/edit/{id}', [App\Http\Controllers\BreedingController::class, 'update'])->name('breeding.update');
 
-Route::get('/show/{id}', [App\Http\Controllers\BreedingController::class, 'show'])->name('breeding.show')->middleware('auth');;
+Route::get('/show/{id}', [App\Http\Controllers\BreedingController::class, 'show'])->name('breeding.show')->middleware('auth');
 
 Route::get('/register', [App\Http\Controllers\Auth\RegisterController::class, 'index'])->name('register');
 
