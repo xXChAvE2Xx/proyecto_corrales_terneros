951c1b811852dab34df4391ceb938f1d1f334db1
Raul Antonio Chavez Aguirre <54995852+xXChAvE2Xx@users.noreply.github.com>
2022-06-14 19:32:54 -0500

    Creacion de modelo proveedor

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Breeding;
 
 class Supplier extends Model
 {
     use HasFactory;
+
+    public function todos(){
+        return $this->hasMany(Breeding::class);
+    } 
+
 }
