31edc3f777887cf55e2939bf9dfdfd01eb5231f1
Raul Antonio Chavez Aguirre <54995852+xXChAvE2Xx@users.noreply.github.com>
2022-06-17 15:35:28 -0500

    Se agrego logica para mostrar los caracteres restantes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
 import "./bootstrap";
+
+let contenido_textarea = "";
+let max = 255;
+
+//Funcion maximo de caracteres en descripcion
+window.valida_num_caracteres = () => {
+    let num_caracteres = document.getElementById("descripcion").value.length;
+    let msg = document.getElementById("msg-caracteres");
+
+    if (num_caracteres >= max) {
+        document.getElementById("descripcion").value = contenido_textarea;
+    } else {
+        contenido_textarea = document.getElementById("descripcion").value;
+        let cant = max - num_caracteres;
+        msg.innerHTML = `${cant} carácteres restantes.`;
+    }
+};
