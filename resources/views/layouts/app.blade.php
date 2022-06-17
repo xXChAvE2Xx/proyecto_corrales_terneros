4072e5354a536aa066fdc9dde5e71c5dbd170f2e
Raul Antonio Chavez Aguirre <54995852+xXChAvE2Xx@users.noreply.github.com>
2022-06-17 15:51:08 -0500

    Se agrego auth necesario para editar cria

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -91,7 +91,7 @@
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                       Cerrar Sesion
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
